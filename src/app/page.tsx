72d1634b8295f732017639818c38ad4f00408fcd
Ragib Al Asad <ragibalasad@gmail.com>
2024-12-04 22:38:59 +0600

    Refinements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,35 @@ export default function Home() {
     <>
       <div className="container mx-auto my-4">
         <div className="grid grid-cols-4 grid-rows-1 gap-8">
-          <div className="p-4 bg-slate-900 rounded text-white">
+          <div className="p-4 bg-zinc-900 rounded text-white ring-1 ring-white/10">
             <p className="mb-1 text-sm text-gray-300">All time</p>
-            <p className="text-3xl text-white">৳ 8,590</p>
-            <p className="mt-1 text-lg text-gray-200">Total Asset</p>
+            <p className="text-3xl text-orange-300">৳ 8,590</p>
+            <div className="flex mt-1">
+              <p className="text-lg text-gray-200">Total Asset</p>
+              <p className="text-base text-green-400 my-auto ml-auto">
+                ৳1,180 in cash
+              </p>
+            </div>
           </div>
 
-          <div className="p-4 bg-slate-900 rounded text-white">
+          <div className="p-4 bg-zinc-900 rounded text-white ring-1 ring-white/10">
             <p className="mb-1 text-sm text-gray-300">This month</p>
             <p className="text-3xl text-red-400">৳ 5,750</p>
-            <p className="mt-1 text-lg text-gray-200">Spend</p>
+            <div className="flex mt-1">
+              <p className="text-lg text-gray-200">Spend</p>
+              <p className="text-base text-gray-200 my-auto ml-auto">
+                ৳191.66 per day
+              </p>
+            </div>
           </div>
 
-          <div className="p-4 bg-slate-900 rounded text-white">
+          <div className="p-4 bg-zinc-900 rounded text-white ring-1 ring-white/10">
             <p className="mb-1 text-sm text-gray-300">December, 2024</p>
             <p className="text-3xl text-green-400">৳ 2,360</p>
             <div className="flex mt-1">
-              <p className="text-lg text-gray-200">Earned</p>
+              <p className="text-lg text-gray-200">Revenue</p>
               <p className="text-base text-gray-200 my-auto ml-auto">
-                ৳1,180 per head
+                ৳78.66 per day
               </p>
             </div>
           </div>
