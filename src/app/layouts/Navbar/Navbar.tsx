06784303dce1710fcc8567ce44065059ca35bee9
Al Asad <ragib420s@gmail.com>
2025-01-02 15:23:39 +0600

    Add icons

--- src/app/layouts/Navbar/Navbar.tsx
+++ src/app/layouts/Navbar/Navbar.tsx
@@ -1,20 +1,38 @@
+import { MdNotifications, MdNotificationsNone } from "react-icons/md";
+import { HiOutlineMenuAlt2 } from "react-icons/hi";
+import { FiSearch } from "react-icons/fi";
+
 const Navbar = () => {
   return (
     <>
-      <div className="fixed w-full bg-zinc-950 p-2">
-        <div className="container mx-auto flex gap-8">
-          <div className="Brand my-auto text-xl text-zinc-100">Quail</div>
-          <div className="Search my-auto w-full">
+      <div className="fixed w-full bg-zinc-950 p-2 text-white">
+        <div className="mx-auto flex gap-8">
+          <div className="Brand flex gap-4 text-xl text-white">
+            <div className="my-auto">
+              <span>Quail</span>
+            </div>
+
+            <div className="my-auto">
+              <HiOutlineMenuAlt2 />
+            </div>
+          </div>
+          <div className="Search relative flex w-full text-white">
+            <div className="absolute p-2.5">
+              <FiSearch className="my-auto text-xl text-zinc-400" />
+            </div>
             <input
               type="text"
-              className="w-full rounded bg-zinc-900 p-2 px-4"
+              className="w-full rounded bg-zinc-900 p-2 px-4 pl-10"
               placeholder="Search..."
             />
           </div>
-          <div className="Profile">
-            <p className="flex size-10 rounded-xl bg-purple-500">
+          <div className="Profile flex gap-4 text-white">
+            <div className="flex text-xl">
+              <MdNotificationsNone className="m-auto" />
+            </div>
+            <div className="flex size-10 rounded-xl bg-purple-500">
               <span className="m-auto text-xl font-medium text-white">S</span>
-            </p>
+            </div>
           </div>
         </div>
       </div>
--- src/app/layouts/SideNav/SideNav.tsx
+++ src/app/layouts/SideNav/SideNav.tsx
@@ -1,22 +1,34 @@
+import {
+  MdSpaceDashboard,
+  MdShoppingCart,
+  MdOutlineSettings,
+} from "react-icons/md";
+
 const SideNav = () => {
   return (
     <main>
       <div className="sticky top-0 -z-10 h-dvh bg-zinc-950 p-2 text-zinc-100">
         <div className="pt-12"></div>
-        <ul className="flex flex-col gap-2">
+        <ul className="flex flex-col gap-2 text-lg">
           <li className="flex size-12 rounded-lg bg-zinc-900">
-            <span className="m-auto">D</span>
+            <span className="m-auto">
+              <MdSpaceDashboard />
+            </span>
+          </li>
+          <li className="flex size-12 rounded-lg">
+            <span className="m-auto">
+              <MdShoppingCart />
+            </span>
+          </li>
+          <li className="flex size-12 rounded-lg">
+            <span className="m-auto">
+              <MdOutlineSettings />
+            </span>
           </li>
           <li className="flex size-12 rounded-lg bg-zinc-900">
             <span className="m-auto">D</span>
           </li>
-          <li className="flex size-12 rounded-lg bg-zinc-900">
-            <span className="m-auto">D</span>
-          </li>
-          <li className="flex size-12 rounded-lg bg-zinc-900">
-            <span className="m-auto">D</span>
-          </li>
-          <li className="flex size-12 rounded-lg bg-zinc-900">
+          <li className="absolute bottom-0 mb-2 flex size-12 rounded-lg bg-zinc-900">
             <span className="m-auto">D</span>
           </li>
         </ul>
